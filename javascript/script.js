5215629e63abeb32d45c7d809396cba9a1a24b9b
Harrison Rogers <harrisonrogers@Harrisons-MacBook-Pro-2.local>
2023-08-19 18:05:55 +1200

    commiting

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+// navbar scroll
+const navbar = document.querySelector('#navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 100) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
